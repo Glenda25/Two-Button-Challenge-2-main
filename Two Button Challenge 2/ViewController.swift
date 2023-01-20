ab5fd3e90a03a1e64c387d29fadcb572ce931d0b
Glenda Mullan <glenda.mullan@gmail.com>
2023-01-20 15:03:52 +1100

    completed

--- Two Button Challenge 2/ViewController.swift
+++ Two Button Challenge 2/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var imageNumber = 1 // variant to iterate thru images
+    
     @IBOutlet weak var messageLabel: UILabel!
     
     @IBOutlet weak var imageViewer: UIImageView!
@@ -20,10 +22,32 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessage(_ sender: UIButton) {
+        print(" Show Message button pressed")
+        messageLabel.text = "Diving is amazing!"
+        messageLabel.textColor = UIColor.green
+        messageLabel.textAlignment = NSTextAlignment.center
+        if imageNumber < 10 {
+            imageNumber = imageNumber + 1
+        }else{
+            imageNumber = 1
+        }
+        print(imageNumber)
+        imageViewer.image = UIImage(named: "image" + String(imageNumber))
     }
     
 
     @IBAction func showAlternativeMessage(_ sender: UIButton) {
+        print(" Show Message button pressed")
+        messageLabel.text = "Life is wonderful!"
+        messageLabel.textColor = UIColor.purple
+        messageLabel.textAlignment = NSTextAlignment.center
+        if imageNumber < 10 {
+            imageNumber = imageNumber + 2
+        }else{
+            imageNumber = 1
+        }
+        print(imageNumber)
+        imageViewer.image = UIImage(named: "image" + String(imageNumber))
     }
 }
 
